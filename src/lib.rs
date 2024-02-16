39eb585023c9a26efaf9f6845a47e416ac4bbddd
Chris Speck <cgspeck@gmail.com>
2024-02-16 18:22:12 +1100

    Work out shape of task defs

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,93 @@
+use clap::ValueEnum;
+use serde::Deserialize;
+
+fn default_ssh_command() -> String {
+    String::from("/usr/bin/ssh")
+}
+
+#[derive(Clone, Debug, Deserialize, PartialEq, Eq)]
+pub struct SshInstructions {
+    #[serde(default = "default_ssh_command")]
+    pub ssh_cmd: String,
+    pub ssh_identity_file: Option<String>,
+    pub ssh_port: Option<u16>,
+    pub ssh_user: Option<String>,
+}
+
+fn default_wakeup_boot_timeout_secs() -> u16 {
+    120
+}
+
+fn default_wakeup_enabled() -> bool {
+    true
+}
+
+fn default_wakeup_validate_ping() -> bool {
+    true
+}
+
+fn default_wakeup_validate_ssh_port() -> bool {
+    true
+}
+
+#[derive(Clone, Debug, Deserialize, PartialEq, Eq)]
 pub struct WakeupInstructions {
+    #[serde(default = "default_wakeup_enabled")]
     pub enabled: bool,
     pub mac: String,
+    #[serde(default = "default_wakeup_boot_timeout_secs")]
     pub boot_timeout_secs: u16,
+    #[serde(default = "default_wakeup_validate_ping")]
     pub validate_ping: bool,
+    #[serde(default = "default_wakeup_validate_ssh_port")]
     pub validate_ssh_port: bool,
 }
 
+fn default_after_shutdown_remote() -> bool {
+    true
+}
+
+fn default_after_shutdown_cmd() -> String {
+    String::from("/usr/bin/shutdown")
+}
+
+fn default_after_validate_shutdown() -> bool {
+    true
+}
+
+fn default_after_shutdown_timeout_secs() -> u16 {
+    120
+}
+
+#[derive(Clone, Debug, Deserialize, PartialEq, Eq)]
 pub struct AfterInstructions {
+    #[serde(default = "default_after_shutdown_remote")]
     pub shutdown_remote: bool,
+    #[serde(default = "default_after_shutdown_cmd")]
     pub shutdown_cmd: String,
+    #[serde(default = "default_after_validate_shutdown")]
     pub validate_shutdown: bool,
+    #[serde(default = "default_after_shutdown_timeout_secs")]
     pub shutdown_timeout_secs: u16,
 }
 
+#[derive(Clone, Debug, Deserialize, PartialEq, Eq, ValueEnum)]
 pub enum ExecutionSide {
     Local,
     Remote,
 }
 
+#[derive(Clone, Debug, Deserialize, PartialEq, Eq)]
 pub struct ProcessInstruction {
     pub execution_side: ExecutionSide,
     pub command: String,
 }
 
+#[derive(Clone, Debug, Deserialize, PartialEq, Eq)]
 pub struct Task {
     pub host: String,
-    pub ssh_port: u16,
-    pub ssh_cmd: String,
+    pub ssh: SshInstructions,
     pub wakeup_instructions: WakeupInstructions,
     pub instructions: Vec<ProcessInstruction>,
-    pub after_instructions: AfterInstructions
+    pub after_instructions: AfterInstructions,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::{fs, path::PathBuf};
+
+use clap::Parser;
+use wakenrun::Task;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[arg(value_name = "FILE")]
+    config: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let config_fp = Cli::parse().config;
+
+    if !config_fp.exists() {
+        panic!("{:?} does not exist!", config_fp)
+    }
+    let data =
+        fs::read_to_string(config_fp.clone()).expect(&format!("Unable to read {:?}", config_fp));
+    let task: Task = serde_yaml::from_str(&data).expect("Unable to open config file");
+    println!("{:#?}", task);
 }
